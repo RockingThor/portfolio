6c26adb6d47ab6494855becc355af59b4acfdd23
Rohit Nandi <rohitnandibmp@gmail.com>
2023-09-15 14:51:40 +0530

    after about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import About from "@/components/about";
+import Divider from "@/components/divider";
 import Intro from "@/components/intro";
 
 export default function Home() {
     return (
         <main className="flex flex-col items-center px-4">
             <Intro />
+            <Divider />
+            <About />
         </main>
     );
 }
---
+++ components/section-heading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SectionHeading = ({ title }: { title: string }) => {
+    return <h2 className="text-3xl font-medium capitalize mb-8">{title}</h2>;
+};
+
+export default SectionHeading;
