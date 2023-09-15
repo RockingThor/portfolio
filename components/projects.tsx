51c1881a7f729f5c3389754057569bdc3625eb6b
Rohit Nandi <rohitnandibmp@gmail.com>
2023-09-15 15:56:29 +0530

    after the project section

---
+++ components/projects.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
+
+const Projects = () => {
+    return (
+        <section className="">
+            <SectionHeading title="My projects" />
+            <div className="">
+                {projectsData.map((project, index) => (
+                    <Project
+                        key={index}
+                        title={project.title}
+                        description={project.description}
+                        tags={project.tags}
+                        imageUrl={project.imageUrl}
+                    />
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Projects;
--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SectionHeading = ({ title }: { title: string }) => {
-    return <h2 className="text-3xl font-medium capitalize mb-8">{title}</h2>;
+    return (
+        <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+            {title}
+        </h2>
+    );
 };
 
 export default SectionHeading;
